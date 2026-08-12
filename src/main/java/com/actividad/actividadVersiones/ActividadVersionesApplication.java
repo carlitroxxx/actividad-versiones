ad8bf8231f033a2bf7f2913075948991481cade8
carlitroxxx <ca.moil@duocuc.cl>
2026-08-12 10:54:10 -0400

    feat(core): version 1.0.0: Creacion de microservicio

---
+++ src/main/java/com/actividad/actividadVersiones/ActividadVersionesApplication.java
@@ -0,0 +1,13 @@
+package com.actividad.actividadVersiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadVersionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadVersionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actividad/actividadVersiones/ActividadVersionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actividad.actividadVersiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadVersionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
